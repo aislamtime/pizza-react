e5a8066ba4d556174d1e3d4433735b0e05a68e88
aislamtime <116115530+aislamtime@users.noreply.github.com>
2023-01-24 20:54:01 +0300

    add main compononts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import './scss/app.scss'
+import { Header } from './components/Header'
+import { Categories } from './components/Categories'
+import { Sort } from './components/Sort'
+import { PizzaBlock } from './components/PizzaBlock'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            <PizzaBlock title={'American'} price={734} />
+            <PizzaBlock title={'Chechen'} price={1005} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
